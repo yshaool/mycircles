b03825e1fb4fec4e316f3577b5eced2655169059
Yaron Shaool <yshaool@gmail.com>
2018-10-26 18:25:30 -0700

    Made changes to mix configuration to create separet vendor file

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/app.js', 'public/js').extract(['vue','bootstrap','jquery','popper.js','lodash'])
    .sass('resources/sass/app.scss', 'public/css');
