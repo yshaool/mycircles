9ab78aa369c3c44a3f024603958f355811b850e8
Yaron Shaool <yshaool@gmail.com>
2018-11-19 17:01:11 -0800

    add reminder to run the right command for testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,3 +8,6 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 }
+
+//to run phpunit inside laravel on windows with xampp nee to use - ./vendor/bin/phpunit
+//the regular php unit runs a globaly installed version
